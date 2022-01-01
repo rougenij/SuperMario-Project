854b2d61e3f3e9231d6a37561054096a3b21158d
Rouge Nij <rougenij@gmail.com>
2022-01-01 23:35:07 +0200

    Trying to fix the movement of Mario

--- src/Utilis/player.js
+++ src/Utilis/player.js
@@ -18,21 +18,30 @@ const playerMove = (e, matrix) => {
       temp[position + 1] = "mario";
     }
   } else if (e.keyCode === 37) {
-    temp[position] = temp[position - 1];
-    temp[position - 1] = "mario";
+    if (position % 20 === 0) {
+      temp[position] = "mario";
+      temp[position - 1] = "sky";
+    } else {
+      temp[position] = temp[position - 1];
+      temp[position - 1] = "mario";
+    }
   } else if (e.keyCode === 38) {
     if (temp[position - 20] === "block" || temp[position - 20] === "lucky") {
       temp[position] = "mario";
     } else {
       temp[position] = temp[position - 40];
       temp[position - 40] = "mario";
+      setTimeout(() => {
+        temp[position] = temp[position + 20];
+        temp[position + 20] = "mario";
+        setTimeout(() => {
+          temp[position] = temp[position + 20];
+          temp[position + 20] = "mario";
+        }, 500);
+      }, 500);
     }
   }
   return temp;
 };
 
 export default playerMove;
-
-// If (temp[postion - 39] === 'block') => then mario can jump and move right
-// if (temp[position- 20] ) === 'block') => Block above mario, he cant jump above it
-// if (temp[position- 20] === 'lucky') ==> Lucky block above mario, he can get a special gift
